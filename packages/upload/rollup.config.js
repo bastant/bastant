16f215f8cafd1a2d177cab16161f193f8edb1637
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2024-01-23 15:26:22 +0100

    cleanup

--- packages/upload/rollup.config.js
+++ packages/upload/rollup.config.js
@@ -1,15 +1,6 @@
 import withSolid from "rollup-preset-solid";
 
-import styles from "rollup-plugin-styles";
-
 export default withSolid({
   input: "src/index.ts",
   targets: ["esm", "cjs"],
-
-  plugins: [
-    styles({
-      mode: "inject",
-      modules: true,
-    }),
-  ],
 });
