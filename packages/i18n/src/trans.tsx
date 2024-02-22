23946a84fc38650c8993a26f360809a8342c5bf4
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2024-02-22 13:00:59 +0100

    create TransFixed component

--- packages/i18n/src/trans.tsx
+++ packages/i18n/src/trans.tsx
@@ -1,8 +1,8 @@
 import type { TOptions } from "i18next";
-import type { ParentComponent } from "solid-js";
+import type { ParentComponent, ParentProps } from "solid-js";
 import { children } from "solid-js";
 import { translateJSX } from "./util.jsx";
-import { useI18n, useTFunc } from "./provider.jsx";
+import { useFixedTFunc, useI18n, useTFunc } from "./provider.jsx";
 
 export type TransProps = { key: string; options?: TOptions };
 
@@ -21,3 +21,33 @@ export const Trans: ParentComponent<TransProps> = (props) => {
     </>
   );
 };
+
+export type TransFixedProps<
+  NS extends string,
+  KP extends string
+> = TransProps & {
+  ns?: NS;
+  keyPrefix?: KP;
+};
+
+export function TransFixed<N extends string, K extends string>(
+  props: ParentProps<TransFixedProps<N, K>>
+) {
+  const t = useFixedTFunc<N, K>(
+    null,
+    () => props.ns,
+    () => props.keyPrefix
+  );
+  const instance = useI18n();
+
+  return (
+    <>
+      {typeof props.children === "string"
+        ? t(props.key, props.children, props.options)
+        : translateJSX(
+            { i18n: instance(), t, props },
+            children(() => props.children)() as Node[]
+          )}
+    </>
+  );
+}
