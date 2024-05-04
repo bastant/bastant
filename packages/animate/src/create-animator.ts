3e3f35d12bf33afa73c148778e2320b6bb0bc6da
Rasmus Kildevaeld <rasmuskildevaeld@gmail.com>
2024-05-04 20:40:31 +0200

    restructure

--- packages/animate/src/create-animator.ts
+++ packages/animate/src/create-animator.ts
@@ -1,10 +1,22 @@
-import { type Accessor, createMemo, createSignal } from "solid-js";
+import { type Accessor, createMemo, createSignal, untrack } from "solid-js";
 
-export function createAnimator<T extends HTMLElement>(element: Accessor<T>) {
-  const [currentAnimation, setCurrentAnimation] = createSignal<Animation>();
 
-  const api = createMemo(() => {
+export interface Animator {
+  cancel(): void,
+  animate(
+    keyframe: Keyframe[] | PropertyIndexedKeyframes,
+    options?: number | KeyframeAnimationOptions
+  ): Promise<void>;
+}
+
+export function createAnimator<T extends HTMLElement>(element: Accessor<T>): Animator {
+
+  const api = createMemo<Animator>((prev) => {
     const el = element();
+
+    prev?.cancel();
+
+    let currentAnimation: Animation | undefined;
 
     return {
       animate: async (
@@ -13,22 +25,32 @@ export function createAnimator<T extends HTMLElement>(element: Accessor<T>) {
       ) => {
         if (!el.isConnected) return;
 
-        currentAnimation()?.cancel();
+        currentAnimation?.cancel();
         const animation = el.animate(keyframe, options);
-        setCurrentAnimation(animation);
+        currentAnimation = animation;
 
         const done = () => {
-          setCurrentAnimation(void 0);
+          currentAnimation = void 0;
         };
 
-        animation.finished.then(done, done);
+        await animation.finished.then(done, done);
       },
       cancel: () => {
-        currentAnimation()?.cancel();
-        setCurrentAnimation(void 0);
+        currentAnimation?.cancel();
+        currentAnimation = void 0;
       },
     };
   });
 
-  return api;
+  return {
+    cancel() {
+      untrack(api).cancel()
+    },
+    async animate(
+      keyframe: Keyframe[] | PropertyIndexedKeyframes,
+      options?: number | KeyframeAnimationOptions
+    ) {
+      await untrack(api).animate(keyframe, options)
+    }
+  }
 }
